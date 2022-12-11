dfe6307bbe5a6ea18512587fdf12b599510e8137
enxa <cezary.walczak.pl@gmail.com>
2022-12-11 20:42:35 +0100

    Refactor

---
+++ components/SummarySelectedCoin.vue
@@ -0,0 +1,13 @@
+<script setup>
+  const selectedCoin = useSelectedCoin()
+
+  console.log(selectedCoin.value)
+</script>
+
+<template>
+  <section class="summary-selected-coin">
+    <div v-if="selectedCoin" class="chart" :key="selectedCoin.id">
+      <SummaryChart :chart="selectedCoin.chart" :id="`select-${selectedCoin.id}`"/>
+    </div>
+  </section>
+</template>
