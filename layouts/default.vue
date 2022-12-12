1ed56c805a04e10bfefef35241d997de698a352c
enxa <cezary.walczak.pl@gmail.com>
2022-12-12 08:02:12 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,12 +8,12 @@
 @import url('https://fonts.cdnfonts.com/css/sf-pro-display');
 
 :root {
-  --color-blackpearl: #0a041c;
+  	--color-blackpearl: #0a041c;
 	--color-manatee: #9896a1;
 	--color-whisper: #ececf4;
 	--color-athensgray: #f7f7f9;
 	--color-white: #ffffff;
-  --color-electricviolet: #7445fb;
+  	--color-electricviolet: #7445fb;
 	--color-shamrock: #2dc78f;
 	--color-algaegreen: #8edec2;
 	--color-applegreen: #e5f5f0;
@@ -22,7 +22,7 @@
 
 	--font-nano-light: 400 1.0rem/1.0rem 'SF Pro Display', sans-serif;
 	--font-tiny-light: 400 1.2rem/1.0rem 'SF Pro Display', sans-serif;
-  --font-small-light: 400 1.4rem/1.4rem 'SF Pro Display', sans-serif;
+  	--font-small-light: 400 1.4rem/1.4rem 'SF Pro Display', sans-serif;
 	--font-small-semibold: 600 1.4rem/1.4rem 'SF Pro Display', sans-serif;
 	--font-semismall-semibold: 600 1.4rem/2.4rem 'SF Pro Display', sans-serif;
 	--font-regular-light: 400 1.6rem/2.4rem 'SF Pro Display', sans-serif;
@@ -70,11 +70,11 @@ textarea {
 	white-space: revert;
 }
 .layout-default {
-  background: var(--color-blackpearl);
-  width: 100%;
-  height: 100vh;
-  display: grid;
-  place-items: center;
-  overflow: hidden;
+	background: var(--color-blackpearl);
+ 	width: 100%;
+ 	height: 100vh;
+	display: grid;
+	place-items: center;
+	overflow: hidden;
 }
 </style>
